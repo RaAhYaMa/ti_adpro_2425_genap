d72443b0b2a6ea3039880664aa975e9102aaf580
yasin <yasinmahendra26@gmail.com>
2025-03-03 18:06:11 +0700

    [RED] Add tests for OrderRepository

--- src/test/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepositoryTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepositoryTest.java
@@ -108,6 +108,16 @@ class OrderRepositoryTest {
     }
 
     @Test
+    void testFindAllByAuthorIfAuthorCorrect() {
+        for (Order order : orders) {
+            orderRepository.save(order);
+        }
+
+        List<Order> orderList = orderRepository.findAllByAuthor(orders.get(1).getAuthor());
+        assertEquals(2, orderList.size());
+    }
+
+    @Test
     void testFindAllByAuthorIfAllLowercase() {
         orderRepository.save(orders.get(1));
 
