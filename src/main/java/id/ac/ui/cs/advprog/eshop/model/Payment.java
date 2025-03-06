25536e8d6bb7e62a436dd100aef17b7591f71d09
yasin <yasinmahendra26@gmail.com>
2025-03-06 12:35:43 +0700

    [GREEN] Implement Payment Class

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -12,7 +12,39 @@ public class Payment {
     private Map<String, String> paymentData;
 
     public Payment(String id, String method, Map<String, String> paymentData) {
+        validateMethod(method);
+        validatePaymentData(paymentData);
+        this.id = id;
+        this.method = method;
+        this.paymentData = paymentData;
     }
 
-    public void setStatus(String status) {}
+    public void validateMethod(String method) {
+        if (method == null) {
+            throw new NullPointerException();
+        }
+        if (method.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public void validatePaymentData(Map<String, String> paymentData) {
+        if (paymentData == null) {
+            throw new NullPointerException();
+        }
+        if (!(paymentData.containsKey("voucherCode") ||
+                paymentData.containsKey("address") && paymentData.containsKey("deliveryFee") ||
+                paymentData.containsKey("bankName") && paymentData.containsKey("referenceCode"))) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public void setStatus(String status) {
+        if (status.equals("SUCCESS") || status.equals("REJECTED")) {
+            this.status = status;
+        }
+        else {
+            throw new IllegalArgumentException();
+        }
+    }
 }
