42f785f33a7745edcc51d00bb470de10b28a4916
yasin <yasinmahendra26@gmail.com>
2025-03-07 19:35:21 +0700

    [RED] make a PaymentRepository skeleton

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -0,0 +1,40 @@
+package id.ac.ui.cs.advprog.eshop.repository;
+
+import id.ac.ui.cs.advprog.eshop.model.Order;
+import id.ac.ui.cs.advprog.eshop.model.Payment;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class PaymentRepository {
+    private List<Payment> paymentList = new ArrayList<>();
+    private Map<String, Order> paymentToOrder = new HashMap<>();
+
+    public Payment addPayment(Order order, String method, Map<String, String> paymentData) {
+        return null;
+    }
+
+    public Payment setStatus(Payment payment, String status) {
+        return null;
+    }
+
+    public Payment getPayment(String paymentId) {
+        return null;
+    }
+
+    public List<Payment> getAllPayments() {
+        return null;
+    }
+
+    public Order getOrder(String paymentId) {
+        return null;
+    }
+
+    public Order findOrderByOrderId(String orderId) {
+        return null;
+    }
+}
